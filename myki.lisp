27eb555d0861f1a669da7d48de0facdf2ac98547
Ankur Kothari <ankz.kothari@gmail.com>
2012-07-14 11:22:53 +1000

    use dynamic programming to quickly calculate best possible fare

--- myki.lisp
+++ myki.lisp
@@ -1,17 +1,39 @@
 #|
  | Calculate the best myki fare for an ordered set of dates
  | Reference brute force solution. Try every possible combination of money and passes.
+ |
+ | Dates are days from reference date; 0 = (monday <= today)
  |#
 
-(defun parts (set)
-  "Return list of all ways of partitioning list (uses exponential time and space)"
+(defun memoize (fn)
+  (let ((cache (make-hash-table :test #'equal)))
+    (lambda (&rest args)
+      (multiple-value-bind 
+        (result exists)
+        (gethash args cache)
+        (if exists
+          result
+          (setf (gethash args cache)
+                (apply fn args)))))))
+
+(defun fare (set)
+  "Calculate best fare for given set of days (max 365), solely using either money or pass"
+  (min
+    ;; money
+    ;; todo: public holiday / weekend daily cap
+    (* 554/100 (length set))
+    ;; pass
+    (let ((days (1+ (- (car (last set)) (first set)))))
+      (if (<= days 7)
+        ;; seven day pass
+        2770/100
+        ;; day passes
+        (* 311/100 (min 325 (max 28 days)))))))
+
+(defun best-fare (set)
   (loop for i from 1 to (length set)
-        for take = (subseq set 0 i)
-        for sub = (parts (nthcdr i set))
-        nconc (if sub
-                (mapcar (lambda (n) (list* take n)) sub)
-                (list (list take)))))
+        minimize (+ (best-fare (nthcdr i set)) (fare (subseq set 0 i)))))
 
-(print (parts (list 'a 'b )))
+(setf (fdefinition 'fare) (memoize #'fare))
+(setf (fdefinition 'best-fare) (memoize #'best-fare))
 
-;; todo: try alternating money,pass,money… and pass,money,pass… for each partition
