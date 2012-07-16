f399a0387a1ef9f9ca04926e14c1b73d6f94fc51
Ankur Kothari <ankz.kothari@gmail.com>
2012-07-16 19:47:52 +1000

    Report cost and method of getting best fare

--- myki.lisp
+++ myki.lisp
@@ -19,15 +19,16 @@
 
 (defun fare (set)
   "Find best fare solely using either money or pass for all given dates"
-  ;; todo: public holiday / weekend daily cap
+  ;; todo: MUST take into account public holidays & weekend daily cap
+  ;; todo: MUST allow choosing zones and concession / full fare
   (let ((money (* 554 (length set)))
         (pass  (let ((days (1+ (- (car (last set)) (first set)))))
                  (if (<= days 7)
                    2770
                    (* 311 (min 325 (max 28 days)))))))
     (if (< money pass)
-      (values money :money)
-      (values pass :pass))))
+      (list money :money)
+      (list pass :pass))))
 
 (defun best-fare (set)
   (loop with lowest = 0
@@ -36,11 +37,11 @@
         for head = (subseq set 0 i)
         for tail = (nthcdr i set)
         for (tail-fare tail-ranges) = (best-fare tail)
-        for head-fare = (fare head)
+        for (head-fare head-fare-type) = (fare head)
         for total-fare = (+ head-fare tail-fare)
         if (or (zerop lowest) (< total-fare lowest)) do
           (setq lowest total-fare
-                ranges (list* head tail-ranges))
+                ranges (list* head-fare-type head tail-ranges))
         finally (return (list lowest ranges))))
 
 (setf (fdefinition 'best-fare) (memoize #'best-fare))
