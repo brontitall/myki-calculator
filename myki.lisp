fbbf6f500f90f60cd6d8a9fd728b7308855ff2a3
Ankur Kothari <ankz.kothari@gmail.com>
2012-07-16 20:14:43 +1000

    Correct myki money weekend fares. Add tests.

--- myki.lisp
+++ myki.lisp
@@ -9,26 +9,32 @@
 (defun memoize (fn)
   (let ((cache (make-hash-table :test #'equal)))
     (lambda (&rest args)
-      (multiple-value-bind
-          (result exists)
+      (multiple-value-bind (result exists)
           (gethash args cache)
         (if exists
           result
           (setf (gethash args cache)
                 (apply fn args)))))))
 
+;; todo: MUST take into account public holidays
+;; todo: MUST allow choosing zones and concession / full fare
+
+(defun money-fare (day)
+  (case (rem day 7)
+    ((5 6) 330) ;; weekend daily cap $3.30
+    (t 554)))   ;; daily zone 1+2
+
 (defun fare (set)
   "Find best fare solely using either money or pass for all given dates"
-  ;; todo: MUST take into account public holidays & weekend daily cap
-  ;; todo: MUST allow choosing zones and concession / full fare
-  (let ((money (* 554 (length set)))
+  (let ((money (reduce #'+ set :key #'money-fare))
         (pass  (let ((days (1+ (- (car (last set)) (first set)))))
                  (if (<= days 7)
                    2770
                    (* 311 (min 325 (max 28 days)))))))
-    (if (< money pass)
-      (list money :money)
-      (list pass :pass))))
+;; todo: SHOULD have :either key as well if no difference
+    (if (< pass money)
+      (list pass :pass)
+      (list money :money))))
 
 (defun best-fare (set)
   (loop with lowest = 0
@@ -46,5 +52,18 @@
 
 (setf (fdefinition 'best-fare) (memoize #'best-fare))
 
-(defun run-test (n)
-  (print (time (best-fare (loop for i below n collect i)))))
+(defun ibest-fare (set)
+  (car (best-fare set)))
+(defun run-tests ()
+  (assert (= 101075 (ibest-fare (loop for i from 0 below 365 collect i))))
+  (assert (= 101075 (ibest-fare (loop for i from 0 below 325 collect i))))
+  (assert (= 46650 (ibest-fare (loop for i from 0 below 150 collect i))))
+  (assert (= 2216 (ibest-fare (list 1 2 8 9))))
+  (assert (= 1992 (ibest-fare (list 1 5 8 9))))
+  (assert (= 0 (ibest-fare nil)))
+  (assert (= 554 (ibest-fare (list 3)))))
+
+;(run-tests)
+
+(defun p (&rest n)
+  (time (best-fare n)))
